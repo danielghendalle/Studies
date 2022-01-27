452c2738580cb574d9f7d7512434b69530700bd5
Daniel Gomes <daniel.coelho@algosoft.com.br>
2022-01-27 15:25:53 -0300

    final aula 4

---
+++ template-strings.js
@@ -0,0 +1,9 @@
+
+const nome = "Daniel";
+const idade = 2022 - 2001;
+const cidadeDeNascimento = "Goiânia";
+// const apresentacao = "meu nome é " + nome + " minha idade é " + idade + " E nasci em : " + cidadeDeNascimento
+
+const apresentacao = `meu nome é ${nome} , minha idade é ${idade}, e nasci em : ${cidadeDeNascimento}`;
+
+console.log(apresentacao)
--- ternary.js
+++ ternary.js
@@ -1,12 +1,18 @@
 //operador ternário
 
+
 const idadeMinima = 18;
-const idadeCliente = 19;
+const idadeCliente = 16;
 
 // if(idadeCliente >= idadeMinima){
 //     console.log("cerveja")
 // }
 // else{
 //     console.log("suco")
-// }
+// }       
+             //condição                    //true    //false
 console.log(idadeCliente >= idadeMinima ? "cerveja" : "suco")
+
+//operador primário -> "="
+//operador secundário -> "=="
+//operador ternário -> "?"
