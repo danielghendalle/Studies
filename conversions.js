aa092ea342905a97444608391a48fb52c6d7bb4b
Daniel Gomes <daniel.coelho@algosoft.com.br>
2022-01-27 11:00:33 -0300

    final aula 2

---
+++ conversions.js
@@ -0,0 +1,14 @@
+//tipo de dado
+//booleans
+
+//conversão implícita
+const numero = 456
+const numeroString = "456"
+
+// console.log(numero == numeroString)
+//Number()
+//String()
+
+console.log(numero + Number(numeroString))
+
+//conversão explícita
