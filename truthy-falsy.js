8ba0cc8a26fcb568d25b674758fb41680a30719e
Daniel Gomes <daniel.coelho@algosoft.com.br>
2022-01-27 10:39:20 -0300

    commit de exercícios

---
+++ truthy-falsy.js
@@ -0,0 +1,30 @@
+//booleano
+
+const usuarioLogado = true
+const contapaga = false
+
+//truthy ou falsy
+
+// 0 => false
+// 1 => true
+
+console.log(0 == false)
+console.log(""== false)
+console.log(1 == true)
+
+//null ==> (vazio ou nada)
+
+let minhaVar;
+let VarNull = null;
+
+console.log(minhaVar)
+console.log(VarNull)
+
+let numero = 3;
+let texto = 'Alura';
+
+console.log(typeof minhaVar)
+console.log(typeof VarNull)
+
+//undefined 
+
