7855750b6014bb7613adced11b5c1a097fafe6b5
Daniel Gomes <daniel.coelho@algosoft.com.br>
2022-01-27 09:22:05 -0300

    exemplos da aula 1

---
+++ type-boolean.js
@@ -0,0 +1,17 @@
+//Tipo booleano
+// true -> verdadeiro
+// false -> falso
+
+const number1 = 5;
+const number2 = 5;
+
+const cadastroAtivado = false;
+
+console.log(number1 === number2)
+
+const text1 = "Alura";
+const text2 = "alura";
+
+const text2Minusculo = text2.toLowerCase();
+
+console.log(text1 === text2Minusculo)
---
+++ type-string.js
@@ -0,0 +1,25 @@
+//Tipo Texto
+
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = "senhaSuperSergura456";
+const StringDeNumeros = "34567";
+
+const citacao = "Meu nome é ";
+const meuNome = "Daniel";
+
+//concatenação (+)
+
+console.log(citacao + meuNome)
+
+//template string ou template literal
+
+
+//diferenciação de maúscular e minúsculas
+
+const cidade = "belo horizonte";
+const input = "Belo Horizonte";
+
+const inputMinusculo = input.toLowerCase();
+
+console.log(cidade === inputMinusculo); // true
